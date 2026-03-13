2b19a5dae27e06500149aa32396135a57b943f03
Deploy User <test@example.com>
2026-03-13 23:46:57 +0300

    Remove duplicate config file with hardcoded localhost from public directory

--- public/app/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'config/config.php';
-require_once 'helpers/session_helper.php';
-
-// Autoload Core Libraries
-spl_autoload_register(function($className){
-    require_once 'core/' . $className . '.php';
-});
